4333ff169a33665ef3fd5940bec3cee31e0d36b0
MacBook <850266645@qq.com>
2016-06-15 10:17:57 +0800

    初始化项目

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/网易新闻/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
