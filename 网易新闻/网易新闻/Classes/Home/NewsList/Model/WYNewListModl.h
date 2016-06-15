c149fdf7b60eee0a2aef53d8cf8aca7c8a8655f0
MacBook <850266645@qq.com>
2016-06-15 15:37:04 +0800

    加载数据模型

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/Model/WYNewListModl.h
@@ -0,0 +1,30 @@
+//
+//  WYNewListModl.h
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYNewListModl : NSObject
+//标题
+@property (nonatomic, copy) NSString *title;
+
+//配图地址
+@property (nonatomic, copy) NSString *imgsrc;
+
+//来源
+@property (nonatomic, copy) NSString *source;
+
+//评论数
+@property (nonatomic, assign) NSInteger replyCount;
+
+//代号
+@property (nonatomic, copy) NSString *docid;
+
+//多图
+@property (nonatomic, strong) NSArray *imgextra;
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/Model/WYNewListModl.m
@@ -0,0 +1,20 @@
+//
+//  WYNewListModl.m
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import "WYNewListModl.h"
+#import <YYModel.h>
+
+@implementation WYNewListModl
+
+- (NSString *)description {
+
+    return [self yy_modelDescription];
+
+}
+
+@end
