029563adfc893b485f57841747c86e41bd253fdf
MacBook <850266645@qq.com>
2016-06-15 20:53:48 +0800

    添加cell

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/View/NewsNormalCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsNormalCell.h
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsNormalCell : UITableViewCell
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/View/NewsNormalCell.m
@@ -0,0 +1,24 @@
+//
+//  NewsNormalCell.m
+//  网易新闻
+//
+//  Created by MacBook on 16/6/15.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import "NewsNormalCell.h"
+
+@implementation NewsNormalCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
