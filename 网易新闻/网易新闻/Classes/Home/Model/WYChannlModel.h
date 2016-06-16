53ae423b301f5e8656ed0bc0bf3ba3e0df5c823a
MacBook <850266645@qq.com>
2016-06-16 23:37:25 +0800

    添加新闻频道模型

---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannlModel.h
@@ -0,0 +1,18 @@
+//
+//  WYChannlModel.h
+//  网易新闻
+//
+//  Created by MacBook on 16/6/16.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYChannlModel : NSObject
+
+//频道名称
+@property (nonatomic,copy) NSString *tname;
+
+//频道ID
+@property (nonatomic,copy) NSString *tid;
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannlModel.m
@@ -0,0 +1,15 @@
+//
+//  WYChannlModel.m
+//  网易新闻
+//
+//  Created by MacBook on 16/6/16.
+//  Copyright © 2016年 MacBook. All rights reserved.
+//
+
+#import "WYChannlModel.h"
+
+@implementation WYChannlModel
+
+
+
+@end
